379f8856eab512b23da2d9475c5b3b7fc1467fda
JB <hog287@gmail.com>
2016-10-08 18:13:51 -0500

    Code clean up
    Cleaned up the code and switched to bytes instead of strings as well as added a option to program the module id with command 12

--- src/nigthfire.cpp
+++ src/nigthfire.cpp
@@ -1,36 +1,52 @@
 /*
- * DEV
- * NightFire slat 16 cue
- *
- * Version 0.0.1
- *
- * Info:
- *  This is the night fire 16 cue dev unit
- *
- * Libraries:
- *  RF24 by TMrh20 (in library manager)
- *  RF24Network by TMrh20 (in library manager)
- *
- *  Pinout:[
- *
- *    ]
- *
-  Packet example:
-    170,2,1,0,0,0
-    170 = start of frame
-    2 = module 2
-    1 = ping command
-    0 = no Datalength
-    0 = no data
-    0 = crc
+* DEV
+* NightFire slat 16 cue
+*
+* Version 0.0.1
+*
+* Info:
+*  This is the night fire 16 cue dev unit
+*
+* Libraries:
+*  RF24 by TMrh20 (in library manager)
+*  RF24Network by TMrh20 (in library manager)
+*
+*  Pinout:[
+*
+*    ]
+*
+Packet example:
+170,2,1,0,0,0
+170 = start of frame
+2 = module 2
+1 = ping command
+0 = no Datalength
+0 = no data
+0 = crc
 
- *
+*
 
-  ToDo:
-  Add display backlight timout.
-  proper send_response commands
+ToDo:
+firing voltage indication.
+add onboard arm and safe switch.
+Hardware:
 
- */
+add good 3.3v regulator.
+swith to arduino micro pro or 32u4?
+swith to rfm69 433mhz(encryption,better packet handling, range)
+add i/o for arm/safe pin
+i2c io expander for menu buttons?
+add 4mbit flash(rfm69 can flash firmware);
+see moteinomega for ideas
+
+
+Done:
+Add display backlight timout
+proper send_response commands
+
++ = done
+
+*/
 
 
 #include <Arduino.h>
@@ -56,34 +72,27 @@ boolean response = false;
 boolean command_recv = false;
 boolean fire_armed_remote = false;
 boolean test_armed_remote = false;
-byte rx_count;
+//byte rx_count;
 
 //Menu variables
-MenuSystem ms;
-Menu mm("   Main Menu");
-MenuItem mm_mi1("Module ID");
-MenuItem mm_mi2("Continuity");
-MenuItem mm_mi3("Connection");
-MenuItem mm_mi4("DEV DIAG");
 
-Menu mu1(" Settings Menu");
-MenuItem mu1_mi1("ID Prog");
-MenuItem mu1_mi2("Wired/Wireless");
 
 
 
 //Module 255 is always the master
-int MODULE_ID  = 1;
-boolean MENU = false;
-boolean eXit = false;
+int MODULE_ID  = EEPROM.read(0);
 
 //serial debug enable
-#define DEBUG
+//#define DEBUG
 
 //System setup
 #define CHANNEL_COUNT 16
-#define FIRE_DELAY 10
+#define FIRE_DELAY 100
 String Version = "0.2";
+
+uint8_t buf[30];
+uint8_t rx_count = 0;
+int packet_error = 0;
 
 //Pin defintions
 #define latchPin 7
@@ -100,25 +109,23 @@ String Version = "0.2";
 //#define fire_enable 4
 
 //buttons
-#define key1 10
-#define key2 11
-#define key3 12
-#define key4 8
+
 
 
 //Protocol define
-#define CMD_UNKNOWN 0
-#define CMD_PING 1
-#define CMD_FIRE 2
+#define CMD_UNKNOWN 0x00
+#define CMD_PING 0x01
+#define CMD_FIRE 0x02
 #define CMD_TEST 3
 #define CMD_TESTALL 4
-#define CMD_FIRE_ENABLE 5
+#define CMD_FIRE_ENABLE 0x05
 #define CMD_FIRE_DISABLE 6
 #define CMD_TEST_ENABLE 7
 #define CMD_TEST_DISABLE 8
 #define CMD_GET_FIRE_SW 9
 #define CMD_GET_TEST_SW 10
 #define CMD_GET_CHANNEL_COUNT 11
+#define CMD_SET_ID 12
 #define RESPONSE_BAD_COMMAND 101
 #define RESPONSE_BUSY 102
 #define RESPONSE_PONG 103
@@ -130,6 +137,7 @@ String Version = "0.2";
 #define RESPONSE_CHANNEL_OUT_OF_RANGE 109
 #define RESPONSE_CHANNEL_COUNT 110
 #define RESPONSE_MODULE_ACTIVE 111
+#define RESPONSE_ID_SET 112
 /*
 //Timecode design
 //255,           00:00:00
@@ -155,6 +163,7 @@ RF24Mesh mesh(radio, network);
 struct recv_packet_type_wireless{
   uint8_t Command;
   uint8_t Module;
+  uint8_t Datalength;
   uint8_t Data;
 };
 
@@ -171,15 +180,15 @@ struct recv_packet_type_serial {
 //Header
 /*
 typedef struct{
-  uint8_t Module;
-  uint8_t Command;
+uint8_t Module;
+uint8_t Command;
 }__attribute__((__packed__))packet_header_t;
 
 typedef struct{
-  packet_header_t Header;
-  uint8_t Datalength;
-  uint8_t Data[];
-  uint8_t CRC;
+packet_header_t Header;
+uint8_t Datalength;
+uint8_t Data[];
+uint8_t CRC;
 }__attribute__((__packed__)) data_packet_t;
 */
 
@@ -187,47 +196,9 @@ typedef struct{
 
 recv_packet_type_serial recv_packet;
 
-void on_linktype_selected(MenuItem* p_menu_item)
-{
 
-}
 
-void on_devdiag_selected(MenuItem* p_menu_item)
-{
 
-}
-
-void on_modprog_selected(MenuItem* p_menu_item)
-{
-
-}
-
-void on_Moduleid_selected(MenuItem* p_menu_item)
-{
-  lcd.setCursor(15,1);
-  //rest
-  lcd.rightToLeft();
-  lcd.print(MODULE_ID);
-  lcd.leftToRight();
-  delay(3000);
-  //Serial.print("module id");
-
-}
-
-void on_Settings_selected(MenuItem* p_menu_item)
-{
-
-}
-
-void on_Contcheck_selected(MenuItem* p_menu_item)
-{
-
-}
-
-void on_Connection_selected(MenuItem* p_menu_item)
-{
-
-}
 
 
 void setup() {
@@ -251,43 +222,28 @@ void setup() {
 
   pinMode(rx_en, OUTPUT);
 
-  //pinMode(key1, INPUT_PULLUP);
-  //pinMode(key2, INPUT_PULLUP);
-  //pinMode(key3, INPUT_PULLUP);
-  //pinMode(key4, INPUT_PULLUP);
+  //LCD timer event
   previousMillis = millis();
-  /*
-  Menu structre
-  Menu mm("  Status Menu");
-  MenuItem mm_mi1("Module ID");
-  MenuItem mm_mi2("Continuity");
-  MenuItem mm_mi3("Connection");
-  MenuItem mm_mi4("DEV DIAG");
 
-  Menu mu1(" Settings Menu");
-  MenuItem mu1_mi1("ID Prog");
-  MenuItem mu1_mi2("Wired/Wireless");
-
-  */
 
 
   lcd.setCursor(6,1);
   lcd.print("V");
   lcd.print(Version);
   delay(3000);
-  mm.add_item(&mm_mi1, &on_Moduleid_selected);
-  mm.add_item(&mm_mi2, &on_Contcheck_selected);
-  mm.add_item(&mm_mi3, &on_Connection_selected);
-  mm.add_item(&mm_mi4, &on_devdiag_selected);
-  mm.add_menu(&mu1);
-  mu1.add_item(&mu1_mi1, &on_Settings_selected);
-  mu1.add_item(&mu1_mi2, &on_linktype_selected);
-  ms.set_root_menu(&mm);
+
 
   //displayMenu();
   lcd.clear();
-  lcd.setCursor(6,1);
-  lcd.print("SAFE");
+  lcd.setCursor(0,0);
+  lcd.print("Status:");
+  lcd.setCursor(9,0);
+  lcd.print("SAFE ");
+  lcd.setCursor(15,1);
+  //rest
+  lcd.rightToLeft();
+  lcd.print(MODULE_ID);
+  lcd.leftToRight();
   //lcd.noBacklight();
   //mesh.begin();
 
@@ -318,10 +274,10 @@ byte CRC8(volatile byte *data, byte len)
 boolean packet_decode()
 {
   int test = 0;
-  test = CRC8(recv_packet.Data, recv_packet.Datalength);
+  test = CRC8((byte*)&recv_packet, sizeof(recv_packet));
   #ifdef DEBUG
-    Serial.print("crc cal = ");
-    Serial.println(test);
+  Serial.print("crc cal = ");
+  Serial.println(test);
   #endif
   if(test == recv_packet.CRC)
   {
@@ -340,95 +296,111 @@ void do_command()
   if (recv_packet.Module == MODULE_ID){
 
 
-  switch (recv_packet.Command)
-  {
-    case CMD_PING:
+    switch (recv_packet.Command)
+    {
+      case CMD_PING:
       //send_response(RESPONSE_PONG);
       //Serial.println("PONG");
       recv_packet.Datalength = 0; //no data is sent just the pong responce command
       send_response(RESPONSE_PONG);
       break;
 
-    case CMD_FIRE:
+      case CMD_FIRE:
+      //Serial.println("Fire!");
       fire_channel();
       break;
 
-    case CMD_TEST:
+      case CMD_TEST:
       //test_channel(recv_packet.Data, recv_packet, Datalength);
       break;
 
-    case CMD_TESTALL:
+      case CMD_TESTALL:
       //test_all_channels();
       break;
 
-    case CMD_FIRE_ENABLE:
+      case CMD_FIRE_ENABLE:
       //not implimented yet
       digitalWrite(testPin, LOW);
       digitalWrite(firePin, HIGH);
-      lcd.clear();
-      lcd.setCursor(4,1);
-      lcd.print("!ARMED!");
+      //lcd.clear();
+      lcd.backlight();
+      lcd.setCursor(9,0);
+      lcd.print("ARMED");
       displayTimer = false;
       fire_armed_remote = true;
       break;
 
-    case CMD_TEST_ENABLE:
+      case CMD_TEST_ENABLE:
       //not implimented yet
       digitalWrite(firePin, LOW);
       digitalWrite(testPin, HIGH);
-      lcd.clear();
-      lcd.setCursor(5,1);
-      lcd.print("!TEST!");
+      //lcd.clear();
+      lcd.backlight();
+      lcd.setCursor(9,0);
+      lcd.print("TEST ");
       displayTimer = false;
       test_armed_remote = true;
       break;
 
-    case CMD_FIRE_DISABLE:
+      case CMD_FIRE_DISABLE:
       //not implimented yet
       digitalWrite(firePin, LOW);
-      lcd.clear();
-      lcd.setCursor(6,1);
-      lcd.print("SAFE");
+      //lcd.clear();
+      lcd.setCursor(9,0);
+      lcd.print("SAFE ");
       //Turn the display timer back on and reset the timer
       displayTimer = true;
       previousMillis = millis();
       fire_armed_remote = false;
       break;
 
-    case CMD_TEST_DISABLE:
+      case CMD_TEST_DISABLE:
       //note implimented yet
       digitalWrite(testPin, LOW);
-      lcd.clear();
-      lcd.setCursor(6,1);
-      lcd.print("SAFE");
+      //lcd.clear();
+      lcd.setCursor(9,0);
+      lcd.print("SAFE ");
       //Turn the display timer back on and reset the timer
       displayTimer = true;
       previousMillis = millis();
       test_armed_remote = false;
       break;
 
-    case CMD_GET_FIRE_SW:
+      case CMD_GET_FIRE_SW:
       //send_response(RESPONSE_FIRE_SW_STATE);
       break;
 
-    case CMD_GET_TEST_SW:
+      case CMD_GET_TEST_SW:
       //send_response(RESPONSE_TEST_SW_STATE);
       break;
 
-    case CMD_GET_CHANNEL_COUNT:
+      case CMD_GET_CHANNEL_COUNT:
       // = CHANNEL_COUNT;
       recv_packet.Datalength = 4;
       recv_packet.Data[0] = CHANNEL_COUNT;
       recv_packet.Data[1] = 1;//Hardware Version
       recv_packet.Data[2] = 0;//Major software Version
-      recv_packet.Data[3] = 2;//Minor software Version
+      recv_packet.Data[3] = 3;//Minor software Version
 
 
-      recv_packet.CRC = CRC8(recv_packet.Data, recv_packet.Datalength);
+      //recv_packet.CRC = CRC8(recv_packet.Data, recv_packet.Datalength);
       send_response(RESPONSE_CHANNEL_COUNT);
       break;
 
-    default:
+      case CMD_SET_ID:
+      EEPROM.write(recv_packet.Data[0],0);
+      MODULE_ID = recv_packet.Data[0];
+      recv_packet.Datalength = 1;
+      recv_packet.Data[0]=EEPROM.read(0);
+      lcd.setCursor(15,1);
+      //rest
+      lcd.rightToLeft();
+      lcd.print(MODULE_ID);
+      lcd.leftToRight();
+      send_response(RESPONSE_ID_SET);
+      break;
+
+      default:
       {
         recv_packet.Datalength = 0;
         send_response(RESPONSE_BAD_COMMAND);
@@ -446,7 +418,7 @@ void sendStructure( char *structurePointer, int structureLength)
 
     Serial.print(structurePointer[i], DEC);
 
-      Serial.print(",");
+    Serial.print(",");
 
 
 
@@ -463,10 +435,11 @@ void send_response(byte response_type)
 
   recv_packet.Module = 255;
   recv_packet.Command = response_type;
-  //Serial.printf("170,%s,%s\n",recv_packet.Module, recv_packet.Command );
-//Serial.write((char * )&recv_packet, sizeof(recv_packet));
-  sendStructure((char *)&recv_packet, sizeof(recv_packet));
-  //Serial.write(recv_packet, sizeof(recv_packet));
+  //CRC the whole packet
+  CRC8((byte*)&recv_packet, sizeof(recv_packet));
+  //write the packet to the serial port
+  Serial.write((char*)&recv_packet, sizeof(recv_packet));
+
 
 
 }
@@ -489,168 +462,129 @@ void fire_channel()
   digitalWrite(latchPin, HIGH);
   delay(FIRE_DELAY);
   digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, 0);
-  shiftOut(dataPin, clockPin, MSBFIRST, 0);
-  digitalWrite(latchPin,HIGH);
-  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, recv_packet.Data[0] - recv_packet.Data[0]);
+  shiftOut(dataPin, clockPin, MSBFIRST, recv_packet.Data[1] - recv_packet.Data[1]);
+  digitalWrite(latchPin, HIGH);
+  //digitalWrite(latchPin, LOW);
 }
 
-void displayMenu() {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  // Display the menu
-  Menu const* cp_menu = ms.get_current_menu();
 
-  //lcd.print("Current menu name: ");
-  lcd.print(cp_menu->get_name());
 
-  lcd.setCursor(0,1);
+void Rx_Decode(){ //  We process sending received on UART.
 
-  lcd.print(cp_menu->get_selected()->get_name());
+  //  For exchange indication on UART.
+  //digitalWrite(TX_RX_LED, HIGH);
+  //time_VD_stop = millis() +  time_set_VD;
+  //TX_RX_SET = 1;
+
+  buf[rx_count] = Serial.read();
+  //Serial.println(rx_bayt, DEC);
+  //recv_packet.header=buf[rx_count];
+
+  if (buf[rx_count] == 0xAA && rx_count ==0)
+  {
+    //CRC8=0;
+    //recv_packet.Header = buf[0];
+    rx_count++;
+  }
+  else if(rx_count == 1)
+  {
+    recv_packet.Module = buf[1];
+    //CRC_8(recv_packet.slat);
+
+    rx_count++;
+  }
+  else if(rx_count == 2)
+  {
+    recv_packet.Command = buf[2];
+    //CRC_8(recv_packet.cue);
+    rx_count++;
+  }
+  else if(rx_count == 3)
+  {
+    recv_packet.Datalength = buf[3];
+    //Serial.println("data length");
+    rx_count++;
+  }else if(rx_count < recv_packet.Datalength+4)
+  {
+    //Serial.println("DAta");
+    recv_packet.Data[rx_count-4] = buf[rx_count];
+    rx_count++;
+  }else if(rx_count == recv_packet.Datalength+4)
+  {
+    recv_packet.CRC = buf[rx_count];
+    if (packet_decode()){
+      do_command();
+    }else{
+
+      //This is just for debuging.
+      packet_error ++;
+      lcd.setCursor(0,0);
+      lcd.println(packet_error,DEC);
+    }
+    #ifdef DEBUG
+      Serial.println("======Packet Start======");
+      Serial.print("Module id = ");
+      Serial.println(recv_packet.Module,DEC);
+      Serial.print("Command = ");
+      Serial.println(recv_packet.Command,DEC);
+      Serial.print("Datalength = ");
+      Serial.println(recv_packet.Datalength,DEC);
+      Serial.print("Data = ");
+
+      for(int i =0; i < recv_packet.Datalength; i++){
+        Serial.print(recv_packet.Data[i],DEC);
+        Serial.print(",");
+      }
+
+      Serial.println();
+      Serial.print("CRC = ");
+      Serial.println(recv_packet.CRC);
+      Serial.println("=======Packet End=======");
+    #endif
+    rx_count = 0;
+  }
+
+
+
 }
 
-int readline(int readch, char *payload, int len)
-{
-  static int pos = 0;
-  int rpos;
-  //Serial.println("Readline called");
 
-  if (readch > 0) {
-    //Serial.println("Got data");
-    switch (readch) {
-      case '\n': // Ignore new-lines
-        //Serial.println("found \n");
-        break;
-      case '\r': // Return on CR
-        rpos = pos;
-        pos = 0;  // Reset position index ready for next time
-        //Serial.println("found \r");
-        return rpos;
-      default:
-        if (pos < len-1) {
-          payload[pos++] = readch;
-          payload[pos] = 0;
-        }
+
+
+
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //mesh update must be called every cycle to keep the radio up
+  //mesh.update();
+  //Serial.print("LOOOOOPING");
+  unsigned long currentMillis = millis();
+  /* Check to see if the displaytimer is enabled. if it is we check to
+  * check to see if we hit the time out. if we do then we want to turn
+  * off the display to save power.
+  */
+  if (displayTimer){
+    if (currentMillis - previousMillis >= interval) {
+      //set the old millis to the current millis so it doesn't get confused
+      lcd.noBacklight();
+      previousMillis = currentMillis;
     }
   }
-  // No end of line has been found, so return -1.
-  return -1;
-}
 
+  digitalWrite(rx_en, LOW);
 
-  void loop() {
-    // put your main code here, to run repeatedly:
-    //mesh update must be called every cycle to keep the radio up
-    //mesh.update();
-    //Serial.print("LOOOOOPING");
-    unsigned long currentMillis = millis();
-    /* Check to see if the displaytimer is enabled. if it is we check to
-     * check to see if we hit the time out. if we do then we want to turn
-     * off the display to save power.
-     */
-    if (displayTimer){
-      if (currentMillis - previousMillis >= interval) {
-        //set the old millis to the current millis so it doesn't get confused
-        lcd.noBacklight();
-        previousMillis = currentMillis;
-      }
-    }else
-    {
-      lcd.backlight();
+  if(Serial.available() > 0) Rx_Decode();
 
-    }
-
-    digitalWrite(rx_en, LOW);
-
-
-    //if (Serial.available() > 0) {
-      static char payload[30];
-      if(readline(Serial.read(), payload, 30) > 0)
-      {
-
-          #ifdef DEBUG
-          digitalWrite(rx_en, HIGH);
-          Serial.print("You entered: >");
-          Serial.print(payload);
-          Serial.println("<");
-
-          #endif
-      //payload = Serial.readString();
-      //TEST+++++++++=++++++++++++++++++++++++++
-           char delimiter[] =",";
-           char* valPosition;
-           int i = 0;
-           int offset = 0;
-           int packed[10];
-           //char* test = payload;
-
-           //This initializes strtok with string to tokenize
-           valPosition = strtok((char*)payload, delimiter);
-           //Serial.println("=======tokenizing==========");
-
-           while(valPosition != NULL){
-             //Serial.println(valPosition);
-             packed[i++] = atoi(valPosition);
-
-             //Here we pass in a NULL value, which tells strtok to continue working with the previous string
-             valPosition = strtok(NULL, delimiter);
-           }
-
-           //Find packet offset
-           for(int y = 0; payload[y] == 170; y++ ){
-             offset++;
-           }
-           recv_packet.Module = packed[1];
-           recv_packet.Command = packed[2];
-           recv_packet.Datalength = packed[3];
-           offset = offset +3;
-           for(int x = 0; x < recv_packet.Datalength; x++ )
-           {
-
-             recv_packet.Data[x] = packed[x+4];
-             //Serial.print("Tick = ");
-             //Serial.println(recv_packet.Data[x]);
-           }
-
-           recv_packet.CRC = packed[recv_packet.Datalength+4];
-          #ifdef DEBUG
-            Serial.println("======Packet Start======");
-            Serial.print("Module id = ");
-            Serial.println(recv_packet.Module);
-            Serial.print("Command = ");
-            Serial.println(recv_packet.Command);
-            Serial.print("Datalength = ");
-            Serial.println(recv_packet.Datalength);
-            Serial.print("Data = ");
-            for(int i =0; i < recv_packet.Datalength; i++){
-              Serial.print(recv_packet.Data[i]);
-              Serial.print(",");
-            }
-
-
-
-            Serial.println();
-
-            Serial.print("CRC = ");
-            Serial.println(recv_packet.CRC);
-            Serial.println("=======Packet End=======");
-
-           #endif
-           if(packet_decode()){
-             do_command();
-           }
-
-
-      //if (packet_decode() == true) do_command();
-
-    }
 
   /*
-while(network.available()){
+  while(network.available()){
   RF24NetworkHeader header;
   network.read(header, &recv_packet, sizeof(recv_packet));
   do_command();
 }
 */
 
-  }
+}
